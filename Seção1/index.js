943a91a654cec338318d02a39530806b54603650
Fernando Longuini <flonguini@outlook.com>
2018-12-21 22:24:47 -0200

    Operadores lógicos

--- Seção1/index.js
+++ Seção1/index.js
@@ -10,3 +10,15 @@
 
  //Comentário
 console.log(olaMundo)
+
+console.log(typeof(olaMundo)) //mostra o tipo de variável
+
+let a = 10;
+const b = "10";
+
+console.log(a==b) // compara dois valores
+console.log(a===b) // compara dois valores e o tipo
+console.log(a!==b) // Diferente de (comparando os tipos também)
+console.log(a!=b) // Diferente de
+console.log(a!=b && typeof(b) == 'string')  // operador and
+console.log(a!=b || typeof(b) == 'string')  // operador or
