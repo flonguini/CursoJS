65ff0b5b70d824702c9b0a21bcb1e6e9e8ebf768
Fernando Longuini <flonguini@outlook.com>
2018-12-21 22:32:10 -0200

    Estrutura if

--- Seção1/index.js
+++ Seção1/index.js
@@ -13,7 +13,7 @@ console.log(olaMundo)
 
 console.log(typeof(olaMundo)) //mostra o tipo de variável
 
-let a = 10;
+let a = 10; // variável com let existe apenas dentro do escopo
 const b = "10";
 
 console.log(a==b) // compara dois valores
@@ -22,3 +22,15 @@ console.log(a!==b) // Diferente de (comparando os tipos também)
 console.log(a!=b) // Diferente de
 console.log(a!=b && typeof(b) == 'string')  // operador and
 console.log(a!=b || typeof(b) == 'string')  // operador or
+
+let cor = 'amarelo';
+
+if (cor ==='verde') {
+    console.log('Siga')
+}else if (cor === 'amarelo'){
+    console.log('Atenção')
+}
+
+else{
+    console.log('Pare')
+}
