60b6e02ec4c6828958e95fa4908fcf2d443aa4bc
Fernando Longuini <flonguini@outlook.com>
2018-12-22 17:23:08 -0200

    Criação da função initialize()

--- Projeto Calculadora/scripts/controller/calcController.js
+++ Projeto Calculadora/scripts/controller/calcController.js
@@ -5,7 +5,20 @@
     constructor()
     {
         this._displayCalc = "0"; // atributo privado
-        this._dataAtual;         // atributo privado
+        this._currentDate;         // atributo privado
+        this.initialize();
+    }
+
+    //Manipulação do DOM
+    initialize(){
+        //seleção dos elementos HTML
+        let displayeCalcEl = document.querySelector("#display") // seleciona o número que está no display da calculadora
+        let dateEl = document.querySelector("#data") // seleciona o número que está na data da calculadora
+        let timeEl = document.querySelector("#hora") // seleciona o número que está na hora da calculadora
+
+        displayeCalcEl.innerHTML = "4567";
+        dateEl.innerHTML = "01/05/2018";
+        timeEl.innerHTML = "00:00";
     }
 
     //Get do field displayCalc
@@ -20,12 +33,11 @@
 
     //Get do field dataAtual
     get dataAtual(){
-        return this._dataAtual;
+        return this._currentDate;
     }
 
     //set do field dataAtual
     set dataAtual(valor){
-        this._dataAtual = valor;
+        this._currentDate = valor;
     }
-
 }
