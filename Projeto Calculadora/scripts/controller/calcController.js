75083eab91760ff1e4a61205fe1b08ce8535b3da
Fernando Longuini <flonguini@outlook.com>
2018-12-22 17:08:07 -0200

    Criação dos getters e setters

--- Projeto Calculadora/scripts/controller/calcController.js
+++ Projeto Calculadora/scripts/controller/calcController.js
@@ -1,10 +1,31 @@
-class CalcController{
+ class CalcController
+ {
     
     //Construtor padrão da classe CalcController
-    constructor(){
-        
-        this.displayCalc = "0";
-        this.dataAtual;
+    constructor()
+    {
+        this._displayCalc = "0"; // atributo privado
+        this._dataAtual;         // atributo privado
+    }
+
+    //Get do field displayCalc
+    get displayCalc(){
+        return this._displayCalc;
+    }
+
+    //Set do field displayCalc
+    set displayCalc(valor){
+        this._displayCalc = valor;
+    }
+
+    //Get do field dataAtual
+    get dataAtual(){
+        return this._dataAtual;
+    }
+
+    //set do field dataAtual
+    set dataAtual(valor){
+        this._dataAtual = valor;
     }
 
 }
