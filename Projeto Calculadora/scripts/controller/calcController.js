4cb2d18ed79bf9b55d62474996c19d9713e79ed9
Fernando Longuini <flonguini@outlook.com>
2018-12-22 23:47:25 -0200

    Data e hora do display

--- Projeto Calculadora/scripts/controller/calcController.js
+++ Projeto Calculadora/scripts/controller/calcController.js
@@ -6,8 +6,18 @@
     //Construtor padrão da classe CalcController
     constructor()
     {
-        this._displayCalc = "0";
+        // The location to display date and time
+        this._locale = 'pt-BR';
+        
+        // Select the DOM elements
+        this._displayCalcEl = document.querySelector('#display'); // Display numbers
+        this._dateEl = document.querySelector('#data'); // Display date
+        this._timeEl = document.querySelector('#hora'); // Display time
+        
+        // The current date
         this._currentDate;
+        
+        // Inicialize the calculator
         this.initialize();
     }
 
@@ -16,14 +26,27 @@
     //#region Methods
 
     initialize(){
-        //Seleciona os elementos do DOM
-        let displayCalcEl = document.querySelector('#display')
-        let dateEl = document.querySelector('#data')
-        let timeEl = document.querySelector('#hora')
 
-        displayCalcEl.innerHTML = "4567";
-        dateEl.innerHTML = "12/12/2012";
-        timeEl.innerHTML = "23:59:54";
+        // Initialize the display date and time
+        this.setDisplayDateTime();
+
+        //Refresh the date and time every 1000ms
+       setInterval(() => {
+            this.setDisplayDateTime(); // call the setDisplayDateTime time
+        }, 1000);
+
+        /*
+        // Pausa o set interval
+        setTimeout(() => {
+            clearInterval(interval); // var interval = setInterval...
+        }, 5000);*/
+    }
+
+    setDisplayDateTime(){
+        //Change the display date 
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        // Change the display time
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
 
     //#endregion
@@ -32,12 +55,22 @@
 
     // Gets the _displayCalc value
     get displayCalc(){
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
 
     // Gets the _currentDate value
-    get actualDate(){
-        return this._currentDate;
+    get currentDate(){
+        return new Date();
+    }
+
+    // Gets the display time
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+
+    // Gets the display date
+    get displayDate(){
+        return this._dateEl.innerHTML;
     }
 
     //#endregion
@@ -46,12 +79,22 @@
 
     // Sets the _displayCalc value
     set displayCalc(value){
-        this._displayCalc = value;
+        this._displayCalcEl.innerHTML = value;
     }
 
     // Sets the _currentDate value
     set actualDate(value){
         this._currentDate = value;
+    }
+
+    // Sets the display time
+    set displayTime(value){
+        this._timeEl.innerHTML = value;
+    }
+
+    // Sets the display date
+    set displayDate(value){
+        this._dateEl.innerHTML = value;
     }
 
     //#endregion
