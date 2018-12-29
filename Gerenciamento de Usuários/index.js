07c6f259815a455929f3b2863171c8e41c06dd37
Fernando Longuini <flonguini@outlook.com>
2018-12-29 19:49:06 -0200

    Criando JSON

--- Gerenciamento de Usuários/index.js
+++ Gerenciamento de Usuários/index.js
@@ -1,26 +1,5 @@
-// Nome do formulário
-let name = document.querySelector('#exampleInputName');
-
-// Seleciona apenas o radio button selecionado
-let gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
-
-// Seleciona a data de nascimento
-let birth = document.querySelector('#exampleInputBirth');
-
-// Seleciona o email
-let email = document.querySelector('#exampleInputEmail');
-
-// Seleciona a senha
-let password = document.querySelector('#exampleInputPassword');
-
-// Seleciona a photo
-let photo = document.querySelector('#exampleInputFile');
-
-// Seleciona o checkbox
-let admin = document.querySelector('#exampleInputAdmin');
-
-// Seleciona todos dos campos que tem a propriedade name dentro do #form-user-create
-var fields = document.querySelectorAll('#form-user-create [name]');
+var fields = document.querySelectorAll("#form-user-create [name]");
+var user = {};
 
 // Faz uma iteração em todos os campos
 fields.forEach(function(field, index){
@@ -28,12 +7,15 @@ fields.forEach(function(field, index){
     if (field.name == 'gender') 
     {
         // verifica se o campo está selecionado
-        if (field.checked) console.log('sim', field);
+        if (field.checked) 
+            user[field.name] = field.value; // adiciona o campo ao json
     }
     else // Caso não esteja selecionado
     {
-        console.log('não');
+        user[field.name] = field.value; // adiciona o campo ao json
     }
     // Exibe no console
-    // console.log(field.name);
+    //  console.log(field.name);
 });
+
+console.log(user);
