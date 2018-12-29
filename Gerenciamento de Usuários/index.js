8c9f5680359d91e50cd6eee07f2fd8ea34b749e1
Fernando Longuini <flonguini@outlook.com>
2018-12-29 20:22:32 -0200

    Criação do evento do formulário

--- Gerenciamento de Usuários/index.js
+++ Gerenciamento de Usuários/index.js
@@ -1,21 +1,29 @@
 var fields = document.querySelectorAll("#form-user-create [name]");
 var user = {};
 
-// Faz uma iteração em todos os campos
-fields.forEach(function(field, index){
-    //Verifica se o campo é o campo gender
-    if (field.name == 'gender') 
-    {
-        // verifica se o campo está selecionado
-        if (field.checked) 
-            user[field.name] = field.value; // adiciona o campo ao json
-    }
-    else // Caso não esteja selecionado
-    {
-        user[field.name] = field.value; // adiciona o campo ao json
-    }
-    // Exibe no console
-    //  console.log(field.name);
-});
 
-console.log(user);
+
+// eventos
+
+document.getElementById("form-user-create").addEventListener("submit", function(event){
+    event.preventDefault(); // cancela o comportamento padrão dos formulário
+    // Faz uma iteração em todos os campos
+    fields.forEach(function(field, index){
+        //Verifica se o campo é o campo gender
+        if (field.name == 'gender') 
+        {
+            // verifica se o campo está selecionado
+            if (field.checked) 
+                user[field.name] = field.value; // adiciona o campo ao json
+        }
+        else // Caso não esteja selecionado
+        {
+            user[field.name] = field.value; // adiciona o campo ao json
+        }
+        // Exibe no console
+        //  console.log(field.name);
+    });
+    
+    console.log(user);
+    console.log('ok');
+});
