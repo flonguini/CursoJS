047032678d2827c1135201eb3292a247de15dbee
Fernando Longuini <flonguini@outlook.com>
2019-01-03 06:17:25 -0200

    Criação das rotas

--- Restful/index.js
+++ Restful/index.js
@@ -3,7 +3,29 @@ const http = require('http');
 let server = http.createServer((req, res) => {
     console.log("URL: ", req.url);
     console.log("METHOD: ", req.method);
-    res.end("okay!");
+    
+    //Criação das rotas
+    switch(req.url){
+        case '/':
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<h1>Olá</h1>');
+            break;
+
+        // Rota localhost:3000/users
+        case '/users':
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({
+            users: [{
+                name: 'hcode',
+                email: 'hcode@hotmail.com',
+                id: 1
+            }]
+        }));
+            
+        break;
+    }
 });
 
 server.listen(3000, '127.0.0.1', ()=>{
