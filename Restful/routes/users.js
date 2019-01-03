8cdc3093977a44ddcf8816f96b6719d45647d7e3
Fernando Longuini <flonguini@outlook.com>
2019-01-03 06:48:33 -0200

    Separação das rotas em arquivos diferentes

---
+++ Restful/routes/users.js
@@ -0,0 +1,25 @@
+let express = require('express');
+
+let routes = express.Router();
+
+routes.get('/', (req,res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        users: [{
+            name: "Fernando",
+            email: "fernando@gmail.com",
+            id: 1
+        }]
+    });
+});
+
+routes.get('/admin', (req, res)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        users: []
+    });
+});
+
+module.exports = routes;
