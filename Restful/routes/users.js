0fce40b8e94a8658b2082aecd97f870cb5f45e49
Fernando Longuini <flonguini@outlook.com>
2019-01-03 22:19:55 -0200

    Validação de dados por meio do express validator

--- Restful/routes/users.js
+++ Restful/routes/users.js
@@ -27,6 +27,8 @@ module.exports = (app) => {
     
     route.post((req, res)=>{
 
+        if (!app.utils.validator.user(app, req,res)) return false;
+
         db.insert(req.body,(err,user)=>{
             if(err){
                 app.utils.error.send(err, req, res);
@@ -49,6 +51,7 @@ module.exports = (app) => {
     });
 
     routeId.put((req,res)=>{
+        if (!app.utils.validator.user(app, req,res)) return false;
         db.update({_id:req.params.id}, req.body, err => {
             if(err){
                 app.utils.error.send(err, req, res);
