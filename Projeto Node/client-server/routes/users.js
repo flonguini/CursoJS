10f2ed96512ab775b29390b6028b0fddd365b981
Fernando Longuini <flonguini@outlook.com>
2019-01-04 00:54:23 -0200

    Instalando e usando Restify para acessar API REST

--- Projeto Node/client-server/routes/users.js
+++ Projeto Node/client-server/routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var restify = require('restify-clients');
+var assert = require('assert');
+
+var client = restify.createJsonClient({
+    url:'http://localhost:4000'
+});
+
+//client.basicAuth('$login', '$password');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    client.get('/users', function(err, request, response, obj){
+        assert.ifError(err);
+        res.end(JSON.stringify(obj,null,2));
+    });
 });
 
 module.exports = router;
