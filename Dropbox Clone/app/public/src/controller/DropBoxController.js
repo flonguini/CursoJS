6406fcddcad70a995e45732fb9081ae073390ec1
Fernando Longuini <flonguini@outlook.com>
2019-01-05 23:11:54 -0200

    Enviando arquivos para o servidor por meio de AJAX

--- Dropbox Clone/app/public/src/controller/DropBoxController.js
+++ Dropbox Clone/app/public/src/controller/DropBoxController.js
@@ -32,7 +32,7 @@ class DropBoxController{
      */
     initEvents(){
 
-        //Adds the click event on btnSendFileEl
+        //Add the click event on btnSendFileEl
         this.btnSendFileEl.addEventListener('click', event => {
 
             //Show the input file window
@@ -40,13 +40,75 @@ class DropBoxController{
 
         });
 
-        // Adds the change event to upload bar
+        // Add the change event to upload bar
         this.inputFilesEl.addEventListener('change', event => {
+
+            // Call the upload task with the selected itens
+            this.uploadTask(event.target.files);
 
             // show the upload bar
             this.snackModalEl.style.display = 'block';
 
         });
+    }
+
+
+    uploadTask(files){
+
+        // collection with primises for each file
+        let promises = [];
+
+        // convert the collection to array
+        [...files].forEach(file => {
+
+            // create a promises for each position inside the array
+            promises.push(new Promise((resolve, reject) => {
+
+                // create new instance of XMLHttpRequest()
+                let ajax = new XMLHttpRequest();
+
+                // send the files by POST 
+                ajax.open('POST', '/upload');
+
+                ajax.onload = event => {
+
+                    try{
+                    
+                        // Resolve the server respost
+                        resolve(JSON.parse(ajax.responseText));
+                    
+                    } catch(e){
+
+                        // Case something wrong happend, reject the promise
+                        reject(e);
+                    }
+
+                    // If onload throw an error
+                    ajax.onerror = event => {
+
+                        // reject the promise
+                        reject(event);
+
+                    }
+
+                };
+
+                // create new instance of FormData
+                let formData = new FormData();
+
+                // append the current file to formData
+                formData.append('input-file', file);
+
+                // send the file to the server
+                ajax.send(formData);
+
+            }));
+
+        });
+
+        // return every resolve for each promises created
+        return Promise.all(promises);
+
     }
 
     //#endregion
