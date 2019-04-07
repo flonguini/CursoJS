508380279b8560cc25b75f8e5c86aaa37d821db8
FERNANDO SILVA <fernando.silva236@fatec.sp.gov.br>
2019-04-06 23:06:18 -0300

    Criação dos elementos  JS pelo id HTML

--- Whatsapp Clone/src/controller/WhatsAppController.js
+++ Whatsapp Clone/src/controller/WhatsAppController.js
@@ -2,5 +2,15 @@ class WhatsAppController{
 
     constructor(){
         console.log('Controller OK');
+
+        this.loadElements();
+    }
+
+    loadElements(){
+
+        this.el = {};
+        document.querySelectorAll('[id]').forEach(element => {
+                this.el[Format.getCamelCase(element.id)] = element;
+        });
     }
 }
